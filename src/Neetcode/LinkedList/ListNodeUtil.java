dfc095f0f6fe5459ec2c4c8e0cc2dc3aa24f10f9
Rubin <lazarettorg@gmail.com>
2025-03-31 17:45:40 +0400

    move the fromArray and printList methods to a separate class

---
+++ src/Neetcode/LinkedList/ListNodeUtil.java
@@ -0,0 +1,25 @@
+package Neetcode.LinkedList;
+
+public class ListNodeUtil {
+    public static ListNode fromArray(int[] values) {
+        if (values == null || values.length == 0) {
+            return null;
+        }
+        ListNode head = new ListNode(values[0]);
+        ListNode current = head;
+        for (int i = 1; i < values.length; i++) {
+            current.next = new ListNode(values[i]);
+            current = current.next;
+        }
+        return head;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
