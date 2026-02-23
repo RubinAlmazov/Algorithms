66f4de4fac3e39535b2897f08a625b4d15210884
Rubin <lazarettorg@gmail.com>
2026-02-23 12:15:32 +0400

    add products of array except self task

---
+++ src/Neetcode/ArraysAndHashing/ProductsOfArrayExceptSelf.java
@@ -0,0 +1,22 @@
+package Neetcode.ArraysAndHashing;
+
+public class ProductsOfArrayExceptSelf {
+    public static void main(String[] args) {}
+
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int prod = 1;
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    prod *= nums[j];
+                }
+            }
+            res[i] = prod;
+        }
+        return res;
+    }
+}
+
